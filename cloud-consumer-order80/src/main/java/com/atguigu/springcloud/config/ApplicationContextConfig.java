a1985b15506391b80aef0f172d9e11dfd6424e7d
lizejin <lizejin@58.com>
2020-07-02 21:49:33 +0800

    集群配置

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
@@ -1,6 +1,7 @@
 package com.atguigu.springcloud.config;
 
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,8 +9,8 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ApplicationContextConfig {
 
-
     @Bean
+    @LoadBalanced//负载均衡功能的开启
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
